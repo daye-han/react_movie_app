d7dd03fb2bd80096800337241b92ff220fda2c91
handaye <handot38@gmail.com>
2021-09-13 00:22:38 +0900

    state, 클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+// import Prototype from 'prop-types';
 
-function App() {
-  return <div>hi</div>;
+class App extends React.Component {
+	state = {
+		isLoading: true,
+		movies: [],
+	};
+
+	componentDidMount() {
+		// 영화 데이터 로딩
+		setTimeout(() => {
+			this.setState({ isLoading: false });
+		}, 6000);
+	}
+
+	render() {
+		const { isLoading } = this.state; //구조분해할당
+		return <div>{isLoading ? 'loading...' : 'we are ready'}</div>;
+	}
 }
 
 export default App;
